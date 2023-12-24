283c5256b786808974347ed95548142ad7725b44
Manish Jangir <111438326+manish201258@users.noreply.github.com>
2023-12-24 10:43:50 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,6 @@
-let a=document.querySelector("#home-button")
-document.addEventListener("DOMContentLoaded", function () {
-    let imageElement = document.getElementById("navi");
-
-    // Retrieve the image source from localStorage
-    let imageUrl = localStorage.getItem("clickedImageSrc");
-
-    // Update the image source
-    if (imageUrl) {
-        imageElement.src = imageUrl;
-    }
-});
-a.addEventListener("click",()=>{
+let clickable=document.querySelectorAll('.clickable');
+clickable.forEach((clicka)=>{
+clicka.addEventListener("click",()=>{
     window.open("index.html", "_self");
+})
 })

27be74c6e3eb88a67ff16fe26718d2b143c03634
Manish Jangir <111438326+manish201258@users.noreply.github.com>
2023-12-24 10:43:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-
-// let a=document.querySelector("#image")
-// let b=document.querySelector("#image").src
-// console.log(b)
-// a.addEventListener("click",()=>{
-//     document.getElementById("navi").src=b;
-//     window.open("image.html")
-// })
-
-document.addEventListener("DOMContentLoaded", function () {
-    // Get all image elements with the class "clickable-image"
-    let images = document.querySelectorAll(".clickable-image");
-
-    // Add click event listener to each image
-    images.forEach((image) => {
-        image.addEventListener("click", handleImageClick);
-    });
-
-    // Reusable function to handle image click event
-    function handleImageClick(event) {
-        let clickedImage = event.target;
-        let imageUrl = clickedImage.src;
-
-        // Store the image source in localStorage
-        localStorage.setItem("clickedImageSrc", imageUrl);
-
-        // Open the image.html page
+let clickable=document.querySelectorAll(".clickable");
+clickable.forEach((image)=>{
+    image.addEventListener("click",()=>{
         window.open("image.html", "_self");
-    }
-});
-
-
+    })
+})
